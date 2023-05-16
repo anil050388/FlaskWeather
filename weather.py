1d32b38818499e7395d8e28e5df1eb0a0829ad0f
anil karnam <anil.karnam03@gmail.com>
2023-05-15 22:51:58 -0400

    Fourth commit

--- weather.py
+++ weather.py
@@ -7,7 +7,6 @@ from config import get_secret
 API = get_secret()
 API_Key = API['MY_KEY']
 
-
 @dataclass
 class WeatherData:
     main: str
@@ -20,9 +19,9 @@ class WeatherData:
     Speed: float
 
 
-def get_lan_lon(city_name, state_code, country_code, API_key):
+def get_lan_lon(city_name, state_code, country_code, API_Key):
     try:
-        resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state_code},{country_code}&appid={API_key}')
+        resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state_code},{country_code}&appid={API_Key}')
         resp.raise_for_status()
         response = resp.json()
         data = response['coord']
@@ -33,8 +32,8 @@ def get_lan_lon(city_name, state_code, country_code, API_key):
         errorcode = 9999
         return errorcode, error
 
-def get_current_weather(lat, lon, API_key):
-    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric').json()
+def get_current_weather(lat, lon, API_Key):
+    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}&units=metric').json()
     data = WeatherData(
         main = resp.get('weather')[0].get('main'),
         description = resp.get('weather')[0].get('description'),
@@ -49,9 +48,9 @@ def get_current_weather(lat, lon, API_key):
 
 def main(city_name, state_name, country_name):
     error = ''
-    lat, lon = get_lan_lon(city_name,state_name,country_name,API_key)
+    lat, lon = get_lan_lon(city_name,state_name,country_name,API_Key)
     if lat != 9999:
-        weather_data = get_current_weather(lat, lon, API_key)
+        weather_data = get_current_weather(lat, lon, API_Key)
     else:
         weather_data = "Error"
     return weather_data
